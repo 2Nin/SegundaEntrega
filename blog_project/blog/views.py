27a699cce688f4c2611e3dd8b4d3de54fc4930f0
Antonio Augusto Carnevalli <antonio.carnevalli@usp.br>
2024-11-17 18:52:16 -0300

    Criar a primeira postagem com as views feitas.

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
 
 def post_list(request):
     posts = Post.objects.all().order_by('-date_posted')
@@ -7,4 +7,19 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.all().order_by('-date_posted')  # Busca os comentários
+    
+    if request.method == 'POST':  # Quando o formulário for enviado
+        content = request.POST.get('content')
+        Comment.objects.create(post=post, author=request.user, content=content)
+        return redirect('post-detail', pk=post.id)
+    
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content)
+        return redirect('post-detail', pk=post.id)
+    return render(request, 'blog/post_form.html')
